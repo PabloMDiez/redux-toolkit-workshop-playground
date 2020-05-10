82640d3989c5c2569b4b32f096d161667b4b01d4
Pablo M. Diez <pablo.diez@typeform.com>
2020-05-10 12:45:09 +0200

    created login and welcome components

---
+++ src/components/login/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const NiceForm = styled.form`
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  width: 200px;
+`
+
+const ErrorMessage = styled.span`
+  color: crimson;
+`
+
+const useInput = () => {
+  const [value, setValue] = useState('')
+  const onChange = event => setValue(event.target.value)
+  return {
+    onChange,
+    value,
+  }
+}
+
+const Login = ({ isLoggingIn, loginError, login }) => {
+  const bindUsername = useInput()
+  const bindPassword = useInput()
+
+  function handleSubmit (event) {
+    event.preventDefault()
+    login({
+      username: bindUsername.value,
+      password: bindPassword.value,
+    })
+  }
+  return (
+    <NiceForm onSubmit={handleSubmit}>
+      <label htmlFor='username'>Username:</label>
+      <input disabled={isLoggingIn} id='username' {...bindUsername} />
+      <label htmlFor='password'>Password:</label>
+      <input disabled={isLoggingIn} id='password' {...bindPassword} />
+      <button disabled={isLoggingIn}>{isLoggingIn ? 'Logging in...' : 'Sign in'}</button>
+      {loginError && <ErrorMessage>Those credentials are not valid</ErrorMessage>}
+    </NiceForm>
+  )
+}
+
+Login.propTypes = {
+  isLoggingIn: PropTypes.bool.isRequired,
+  loginError: PropTypes.bool.isRequired,
+  login: PropTypes.func.isRequired,
+}
+
+export default Login
