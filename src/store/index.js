d038db91ea1e894c90fa22c3a0a54959d1d5aa53
Pablo M. Diez <pablo.diez@typeform.com>
2020-05-10 12:45:30 +0200

    create user domain in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import ui from './ui'
+import user from './user'
 
 const rootReducer = combineReducers({
   ui,
+  user,
 })
 
 const store = configureStore({
---
+++ src/store/user/index.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit'
+import { apiLogin } from '../../lib/api'
+
+export const login = createAsyncThunk('users/login',
+  async ({ username, password }) => apiLogin({ username, password }))
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    error: false,
+    loading: false,
+    currentUser: null,
+  },
+  extraReducers: {
+    [login.pending]: state => {
+      state.loading = true
+    },
+    [login.rejected]: state => {
+      state.loading = false
+      state.error = true
+    },
+    [login.fulfilled]: (state, { payload }) => {
+      state.loading = false
+      state.error = false
+      state.currentUser = payload
+    },
+  }
+})
+
+const getUserDomain = state => state.user
+
+export const getCurrentUser = createSelector(getUserDomain, state => state.currentUser)
+export const isLoggingIn = createSelector(getUserDomain, ({ loading }) => loading)
+export const hasLoginError = createSelector(getUserDomain, ({ error }) => error)
+
+export const getCurrentUserName = createSelector(getCurrentUser, user => user.name)
+export const isLoggedIn = createSelector(getCurrentUser, user => !!user)
+
+export default userSlice.reducer
