1111a4a439d17171b49466f05f9451b4e537c38d
Pablo M. Diez <pablo.diez@typeform.com>
2020-05-18 16:46:54 +0200

    feat: use createStructuredSelector

--- src/containers/layout/grid.js
+++ src/containers/layout/grid.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import Grid from '../../components/layout/grid'
 import { getDarkMode } from '../../store/ui'
 
-const mapStateToProps = state => ({
-  darkMode: getDarkMode(state),
+const mapStateToProps = createStructuredSelector({
+  darkMode: getDarkMode,
 })
 
 export default connect(mapStateToProps)(Grid)
